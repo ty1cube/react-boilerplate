b3e6b35eaac77ff5adfad9623d81e61a069ca175
Azeez Akinsola <harkzed@gmail.com>
2019-05-23 17:48:40 -1200

    react boilerplate initial commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const App = function(){
+    return <h1>This is the app test component</h1>;
+};
+
+ReactDOM.render(<App/>, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
+module.exports = {
+
+    entry: path.join(__dirname, 'src', 'app.js'),
+
+    output: {
+
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.bundle.js'
+    },
+    mode: process.env.NODE_ENV||'development',
+
+    module:{
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                  }
+            }
+        ]
+    },
+
+    devServer: {
+        contentBase: path.join(__dirname, 'src')
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'index.html')
+        })
+    ]
+
+};
+
